fd5c48d7e86b7d93fa819f67387d5f56f4be3008
kevingilfether <kevingilfether@gmail.com>
2017-09-27 11:47:27 -0400

    Added a conditional statement

--- Week1_day3/Program.cs
+++ Week1_day3/Program.cs
@@ -10,6 +10,22 @@ namespace Week1_day3
     {
         static void Main(string[] args)
         {
+            // Conditionals - If then, if else, etc.
+
+            int firstNumber = 2;
+            int secondNumber = 6;
+
+            if(firstNumber > secondNumber)
+            {
+                // Do Something
+                Console.WriteLine("This is an untrue expression. You messed up.");
+            }
+            else
+            {
+                Console.WriteLine("That makes sense.");
+            }
+
+
         }
     }
 }
