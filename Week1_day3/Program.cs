1c9acffb7c117e209a29ad100daac87fdd96d03c
kevingilfether <kevingilfether@gmail.com>
2017-09-27 13:39:53 -0400

    Practice with age if-else

--- Week1_day3/Program.cs
+++ Week1_day3/Program.cs
@@ -41,33 +41,33 @@ namespace Week1_day3
             //    Console.WriteLine("Bloop. (You got it.)");
             //}
 
-            Console.WriteLine("How old are you?");
-            int userAge = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How old are you?");
+            //int userAge = int.Parse(Console.ReadLine());
 
-            if(userAge >= 0 && userAge <= 1)
-            { 
-                Console.WriteLine("You're just a baby!");
-            }
-            else if(userAge > 1 && userAge < 4)
-            {
-                Console.WriteLine("Yay toddlers!");
-            }
-            else if (userAge >=4 && userAge < 13)
-            {
-                Console.WriteLine("Enjoy your childhood");
-            }
-            else if(userAge >=13 && userAge < 65)
-            {
-                Console.WriteLine("Life is awkward.");
-            }
-            else if(userAge >= 65 && userAge < 100)
-            {
-                Console.WriteLine("Enjoy your retirement.");
-            }
-            else
-            {
-                Console.WriteLine("That's not really your age...");
-            }
+            //if(userAge >= 0 && userAge <= 1)
+            //{ 
+            //    Console.WriteLine("You're just a baby!");
+            //}
+            //else if(userAge > 1 && userAge < 4)
+            //{
+            //    Console.WriteLine("Yay toddlers!");
+            //}
+            //else if (userAge >=4 && userAge < 13)
+            //{
+            //    Console.WriteLine("Enjoy your childhood");
+            //}
+            //else if(userAge >=13 && userAge < 65)
+            //{
+            //    Console.WriteLine("Life is awkward.");
+            //}
+            //else if(userAge >= 65 && userAge < 100)
+            //{
+            //    Console.WriteLine("Enjoy your retirement.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("That's not really your age...");
+            //}
            
 
 
